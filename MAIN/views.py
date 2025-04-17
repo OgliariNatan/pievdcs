38dcbe396241c41fad00a4c89682efd7dbd607a4
OgliariNatan <natanogliari@gmail.com>
2025-04-17 16:20:37 -0300

    adicionado arquivos de configurações

--- MAIN/views.py
+++ MAIN/views.py
@@ -2,4 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "base/home.html")
+    context = {
+        "title": "Página Inicial",
+        "welcome_message": "Bem-vindo à Plataforma Integrada de Enfrentamento à Violência Doméstica e Crimes Sexuais!"
+    }
+    return render(request, "home.html", context)
+
+def index(request):
+    context = {
+        "title": "Bem-vindo",
+        "description": "Plataforma Integrada de Enfrentamento à Violência Doméstica e Crimes Sexuais"
+    }
+    return render(request, "index.html", context)
