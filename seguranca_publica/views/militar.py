2f18482bc838129846af39689a2f5dcd060601ea
OgliariNatan <natanogliari@gmail.com>
2025-04-23 09:08:28 -0300

    adicionado templetes de PM PP

--- seguranca_publica/views/militar.py
+++ seguranca_publica/views/militar.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def militar(request):
-    return HttpResponse("Página Militar")
+    return render(request, "militar.html")
