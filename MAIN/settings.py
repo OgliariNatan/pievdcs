e65fd3d45de4432b947e93d72f41683cee40f09c
OgliariNatan <natanogliari@gmail.com>
2025-05-02 10:59:42 -0300

    adicionado recuperação de senha

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -49,6 +49,16 @@ INSTALLED_APPS = [
     
 ]
 
+
+#E-mail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # E-mail do remetente
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Senha ou token do remetente
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
