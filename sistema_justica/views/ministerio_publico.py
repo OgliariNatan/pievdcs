f39ed5bc33d247159d7490a1b87c79ad06e40df1
OgliariNatan <natanogliari@gmail.com>
2025-05-19 11:15:44 -0300

    adicionado autorização por grupos de usuarios

--- sistema_justica/views/ministerio_publico.py
+++ sistema_justica/views/ministerio_publico.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.models import Group
+from django.http import HttpResponseForbidden
 
+
+
+def grupos_permitidos(Group):
+    def decorator(view_func):
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.groups.filter(name__in=Group).exists():
+                return view_func(request, *args, **kwargs)
+            return HttpResponseForbidden("Você não tem permissão para acessar esta página. <br> Entre em contato com o administrador do sistema.")
+        _wrapped_view.__name__ = view_func.__name__
+        return _wrapped_view
+    return decorator
+
+@login_required(login_url=reverse_lazy('login'))
+@grupos_permitidos(['Ministério Público', 'Policia Penal', 'Poder Judiciário'])
 def ministerio_publico(request):
     contexto = {
         'title': 'Ministério Público',
         'description': 'This page provides information about the public ministry.',
         'encaminhamentos': 5,
         'notificacoes': 2,
+        'user': request.user,
     }
     return render(request, "ministerio_publico.html", contexto)
--- sistema_justica/views/poder_judiciario.py
+++ sistema_justica/views/poder_judiciario.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
+@login_required(login_url=reverse_lazy('login'))
 
 def poder_judiciario(request):
     contexto = {
@@ -6,5 +10,6 @@ def poder_judiciario(request):
         'description': 'This page provides information about the judicial power.',
         'encaminhamentos': 5,
         'notificacoes': 4,
+        'user': request.user,
     }
     return render(request, "poder_judiciario.html", contexto)
