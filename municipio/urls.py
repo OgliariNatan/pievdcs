2fb8db5da2a1c41e127262600a128ea9c033654b
OgliariNatan <natanogliari@gmail.com>
2025-05-20 10:40:31 -0300

    adicionado a viualização a secretaria de saúde

--- municipio/urls.py
+++ municipio/urls.py
@@ -3,6 +3,7 @@ from .view.conselho_tutelar import conselho_tutelar
 from .view.creas import creas
 from .view.cras import cras
 from .view.caps import caps
+from .view.secretaria_saude import secretaria_saude
 
 
 app_name = 'municipio'
@@ -13,4 +14,5 @@ urlpatterns = [
     path('creas/', creas, name='creas'),
     path('cras/', cras, name='cras'),
     path('caps/', caps, name='caps'),
+    path('secretaria_saude/', secretaria_saude, name='secretaria_saude'),
 ]
---
+++ municipio/view/secretaria_saude.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from .permission_group import grupos_permitidos
+
+@login_required(login_url=reverse_lazy('login'))
+@grupos_permitidos(['Secretaria de Saúde'])
+
+def secretaria_saude(request):
+    contexto = {
+        'title': 'Secretaria de Saúde',
+        'description': 'This page provides information about the Secretaria de Saúde system.',
+        'encaminhamentos': 2,  # Criar variaveis para encaminhamentos
+        'notificacao': 1,
+        'user' : request.user,
+    }
+    return render(request, "secretaria_saude.html", contexto)
