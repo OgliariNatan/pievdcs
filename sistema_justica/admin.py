a8ec4be2f14c005995db505e650d867212a1c7eb
OgliariNatan <natanogliari@gmail.com>
2025-05-24 15:54:21 -0300

    adicionando modelos de dados

--- sistema_justica/admin.py
+++ sistema_justica/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models.base import Vitima_dados
 
 # Register your models here.
+admin.site.register(Vitima_dados)
---
+++ sistema_justica/models/base.py
@@ -0,0 +1,101 @@
+from django.db import models
+from seguranca_publica.models import *
+from usuarios.models import Vitima
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+sexo_choices = [
+    ("M", "Masculino"),
+    ("F", "Feminino"),
+    ("O", "Outro"),
+]
+
+nacionalidade_choices = [
+    ("BR", "Brasileiro"),
+    ("PT", "Português"),
+    ("ES", "Espanhol"),
+    ("EN", "Inglês"),
+    ("FR", "Francês"),
+    ("IT", "Italiano"),
+    ("DE", "Alemão"),
+    ("CN", "Chinês"),
+    ("JP", "Japonês"),
+    ("IN", "Indiano"),
+]
+
+
+class Vitima_dados(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(
+        max_length=250, 
+        unique=True, 
+        verbose_name="Nome Completo da Vítima", 
+        null=False, blank=False,
+        help_text="Nome completo da vítima",
+    )
+    cpf = models.CharField(
+        max_length=14,  # 000.000.000-00
+        unique=False,
+        verbose_name="CPF da Vítima",
+        null=False, blank=False,
+        help_text="Formato: 000.000.000-00",
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name="Data de Nascimento da Vítima", 
+        unique=True, null=False, blank=False,
+        help_text="Data de nascimento da vítima",
+    )
+    sexo = models.CharField(
+        max_length=1,
+        choices=sexo_choices,
+        verbose_name="Sexo da Vítima",
+        null=False, blank=False,
+        help_text="Escolha o sexo da vítima",
+    )
+    idade = models.PositiveIntegerField(null=True, blank=True, editable=False)
+    nacionalidade = models.CharField(
+        max_length=2, 
+        verbose_name="Nacionalidade da Vítima", 
+        choices=nacionalidade_choices, 
+        default="BR",
+        null=False, blank=False,
+        help_text="Escolha a nacionalidade da vítima",
+    )  
+
+    endereco = models.CharField(max_length=255, verbose_name="Endereço da Vítima")
+    telefone = models.CharField(max_length=15, verbose_name="Telefone da Vítima", help_text="Formato: (DDD) XXXXX-XXXX")
+    email = models.EmailField(verbose_name="Email da Vítima", unique=True, null=False, blank=False)
+    usuario = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        null=True, blank=True,
+        verbose_name="Usuário",
+        help_text="Usuário Responsável pelos dados da vítima",
+        )
+
+    def save(self, *args, **kwargs):
+        # Calcula a idade antes de salvar
+        today = timezone.now().date()
+        if self.data_nascimento:
+            self.idade = today.year - self.data_nascimento.year - (
+                (today.month, today.day) < (self.data_nascimento.month, self.data_nascimento.day)
+            )
+        else:
+            self.idade = None
+
+        # Remove qualquer caractere não numérico do CPF
+        cpf_digits = ''.join(filter(str.isdigit, self.cpf))
+        # Formata como 000.000.000-00 se o comprimento for 11
+        if len(cpf_digits) == 11:
+            self.cpf = f"{cpf_digits[:3]}.{cpf_digits[3:6]}.{cpf_digits[6:9]}-{cpf_digits[9:]}"
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Dados da Vítima"
+        verbose_name_plural = "Dados das Vítimas"
