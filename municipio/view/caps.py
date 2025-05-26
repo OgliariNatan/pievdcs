9cbd63dfbf5d569b2179ca509307c1522a71cd32
OgliariNatan <natanogliari@gmail.com>
2025-05-26 11:09:03 -0300

    u

--- municipio/view/caps.py
+++ municipio/view/caps.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.urls import reverse_lazy
 from .permission_group import grupos_permitidos
 
-
+#@permission_required('CAPS.view_caps', login_url=reverse_lazy('login'))
 @login_required(login_url=reverse_lazy('login'))
 @grupos_permitidos(['CAPS'])
 def caps(request):
