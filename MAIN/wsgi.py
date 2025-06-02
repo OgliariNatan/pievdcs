ffe19b20024762fc062dc26ea926cd77a6b7ba2a
OgliariNatan <natanogliari@gmail.com>
2025-06-02 18:10:38 -0300

    comentado o wsgi.py

--- MAIN/wsgi.py
+++ MAIN/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MAIN.settings")
 
 application = get_wsgi_application()
+
+#chdir = os.path.dirname(os.path.abspath(__file__))
+#env = r"C:\\Users\\AULA-1\\Documents\\GitHub\\pievdcs\\virtual_env\\Scripts\\activate"
