0fd766790073af175666a1a6d316a6df7d1dd6b1
OgliariNatan <natanogliari@gmail.com>
2025-06-21 17:46:37 -0300

    ajustando visualização

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.messages",# Mensagens de feedback
+    "django.contrib.staticfiles",# Arquivos estáticos
     'corsheaders', # Para acesso cruzado
     'dbbackup', #Para realizar backup
     'smart_selects',#Para selecionar os municipios
@@ -70,7 +70,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',  # Middleware para CORS
 ]
+
+
 
 ROOT_URLCONF = "MAIN.urls"
 
