f955b911eccdb481ab166d92e774ff211acfb608
OgliariNatan <natanogliari@gmail.com>
2025-06-25 11:33:09 -0300

    estilizando cadastros

--- sistema_justica/forms/cadastros.py
+++ sistema_justica/forms/cadastros.py
@@ -13,7 +13,7 @@ class CadastroVitimaForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(CadastroVitimaForm, self).__init__(*args, **kwargs)
         self.fields['estado'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
-        self.fields['municipio'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
+        self.fields['municipio'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text', 'placeholder': 'Selecione o estado antes'})
         self.fields['data_nascimento'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'date'})
     
 class CadastroAgressorForm(forms.ModelForm):
@@ -22,10 +22,12 @@ class CadastroAgressorForm(forms.ModelForm):
         fields = '__all__'      
         widgets = {
             'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
-            #'estado': forms.Select(attrs={'id': 'id_estado'}),
-            #'municipio': forms.Select(attrs={'id': 'id_municipio'}),
         }
-
+        def __init__(self, *args, **kwargs):
+            super(CadastroAgressorForm, self).__init__(*args, **kwargs)
+            self.fields['estado'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
+            self.fields['municipio'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
+            self.fields['data_nascimento'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'date'})
 class CadastroMunicipioForm(forms.ModelForm):
     class Meta:
         model = Municipio
@@ -40,9 +42,13 @@ class CadastroFilhosForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'data_nascimento': forms.DateInput(attrs={'type': 'date'}),
-            #'estado': forms.Select(attrs={'id': 'id_estado'}),
-            'municipio': forms.Select(attrs={'id': 'id_estado'}),
         }
+
+        def __init__(self, *args, **kwargs):
+            super(CadastroFilhosForm, self).__init__(*args, **kwargs)
+            self.fields['estado'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
+            self.fields['municipio'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'text'})
+            self.fields['data_nascimento'].widget.attrs.update({'class': 'form-control, form-control-sm', 'type': 'date'})
 
 
 
