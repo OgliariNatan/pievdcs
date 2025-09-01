fa385f8ade9999774f73bb615cadc0e8335e85b2
ogliarinatan <natanogliari@gmail.com>
2025-09-01 15:21:38 -0300

    ajustado o setting e o env para o ambiente de produçao

--- MAIN/settings.py
+++ MAIN/settings.py
@@ -52,12 +52,12 @@ INSTALLED_APPS = [
 
 
 #E-mail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True).lower() in ['true', '1', 't']
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # E-mail do remetente
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Senha ou token do remetente
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+# EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True).lower() in ['true', '1', 't']
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # E-mail do remetente
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Senha ou token do remetente
 
 
 MIDDLEWARE = [
@@ -84,7 +84,9 @@ SMART_SELECTS_INCLUDE_JQUERY = True
 CSRF_TRUSTED_ORIGINS = [
     'https://www.redecontraaviolencia.org',
     'https://redecontraaviolencia.org',
-    'http://62.72.9.77'
+    'http://62.72.9.77',
+    'http://www.redecontraaviolencia.org',
+    'http://redecontraaviolencia.org'
 ]
 
 
