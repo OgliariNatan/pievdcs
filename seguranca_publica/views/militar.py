66fe3f0ee0c961cc101b2b48cfcb745adc54851d
ogliarinatan <natanogliari@gmail.com>
2025-09-11 20:06:48 -0300

    Ajustando mensageiro das paginas

--- seguranca_publica/views/militar.py
+++ seguranca_publica/views/militar.py
@@ -2,14 +2,21 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from .permission_group import grupos_permitidos
+from mensageria.models import Notificacao, StatusNotificacao
+from mensageria.utils import enviar_notificacao_usuario, enviar_notificacao_grupo
+from usuarios.models import CustomUser, CustomGroup
+from MAIN.decoradores.calcula_tempo import calcula_tempo
 
 @login_required(login_url=reverse_lazy('login'))
 @grupos_permitidos(['Polícia Militar'])
+@calcula_tempo
 def militar(request):
+    notificacoes_nao_lidas = Notificacao.contar_nao_lidas_usuario(request.user)
+
     contexto = {
         'title': 'Polícia Militar',
         'encaminhamentos': 5,
-        'alert': 2,
+        'alert': notificacoes_nao_lidas,
         'description': 'This page provides information about the militar system.',
         'user' : request.user,
     }
