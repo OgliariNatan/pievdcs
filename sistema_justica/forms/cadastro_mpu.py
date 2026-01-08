1f04824626c636e235fa1f78c4497fe52a533507
OgliariNatan <natanogliari@gmail.com>
2026-01-08 11:37:04 -0300

    [sistema_justica] refactor: selecao multipla do tipo de violencia nos cadastros

--- sistema_justica/forms/cadastro_mpu.py
+++ sistema_justica/forms/cadastro_mpu.py
@@ -19,7 +19,9 @@ class CadastroMedidaProtetiva(forms.ModelForm):
                 'class': 'form-control form-control-sm border border-gray-400 rounded-xl'
             }),
             
-            'tipo_de_violencia': forms.CheckboxSelectMultiple(),
+            'tipo_de_violencia': forms.CheckboxSelectMultiple(
+                attrs={'class': 'form-check-input'}
+            ),
 
             'solicitada_mpu': ToggleSwitchWidget(size='xs',
                 active_color=cor_ativa_toggle,
